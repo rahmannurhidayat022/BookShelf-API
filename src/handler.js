cd40b452e0b0a67742ed6e7f5db6614edcdf93ec
Rahman Nurhidayat <rahmannurhidayat022@gmail.com>
2021-07-20 07:57:56 +0700

    delete books berhasil

--- src/handler.js
+++ src/handler.js
@@ -162,7 +162,26 @@ const editBooksByIdHandler = (request, h) => {
   return response;
 };
 
-const deleteBooksByIdHandler = () => {};
+const deleteBooksByIdHandler = (request, h) => {
+  const { bookId } = request.params;
+
+  const index = books.findIndex((book) => book.id === bookId);
+
+  if (index !== -1) {
+    books.splice(index, 1);
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil dihapus',
+    }).code(200);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku gagal dihapus. Id tidak ditemukan',
+  }).code(404);
+  return response;
+};
 
 module.exports = {
   addBooksHandler,
